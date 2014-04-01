f594f27ae1cbbcb6386d400f70585fe651a25600
vagrant <vagrant@student-dev-box>
2014-04-01 18:21:58 +0000

    User now takes a name as an argument on initialize. sets win count and matches played to 0. auto generates id.

--- lib/RPS-EXTREME/user.rb
+++ lib/RPS-EXTREME/user.rb
@@ -1,5 +1,14 @@
 module RPS
   class User
-
+    attr_reader :id, :name
+    attr_accessor :win_count, :matches_played
+    @@counter = 0
+    def initialize(name)
+      @name = name
+      @@counter += 1
+      @id = @@counter
+      @win_count = 0
+      @matches_played = 0
+    end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,27 @@
 require 'spec_helper'
 
 describe RPS::User do
+  before do
+    RPS::User.class_variable_set(:@@counter, 0)
+    @user = RPS::User.new('fernando')
+  end
   it 'exists' do
     expect(RPS::User).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it 'auto generates an id' do
+      expect(@user.id).to eq(1)
+    end
+    it 'takes a name as an argument' do
+      expect(@user.name).to eq('fernando')
+    end
+    it 'sets win count to 0' do
+      expect(@user.win_count).to eq(0)
+    end
+    it 'sets matches played to 0' do
+      expect(@user.matches_played).to eq(0)
+    end
+  end
+
 end
